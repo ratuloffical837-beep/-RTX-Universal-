aa797e3449aa2aa2c1e76aecc1c1693849134c29
ratuloffical837-beep <ratuloffical837@gmail.com>
2026-06-23 09:23:53 +0600

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+/**
+ * RTX SMM Panel - Component Exports
+ */
+
+export { UserProfile } from './UserProfile';
+export { BalanceCard } from './BalanceCard';
+export { DepositSection } from './DepositSection';
+export { OrderSection } from './OrderSection';
+export { OrderHistory } from './OrderHistory';
+export { DepositHistory } from './DepositHistory';
+export { SupportButton } from './SupportButton';
+export { SystemLogs } from './SystemLogs';
+export { LoadingScreen } from './LoadingScreen';
+export { Footer } from './Footer';
